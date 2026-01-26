c82ba771e1f85d9dfaa54c1bce208d1485062a89
david-marcoss <ddragondavi@gmail.com>
2026-01-26 16:09:43 -0300

    feat: implementation page types

--- Routes.tsx
+++ Routes.tsx
@@ -1,21 +1,67 @@
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { NavigationContainer, RouteProp } from "@react-navigation/native";
+import {
+  createNativeStackNavigator,
+  NativeStackNavigationProp,
+} from "@react-navigation/native-stack";
 import HomeScreen from "./screens/Home";
 import DetailScreen from "./screens/Detail";
+import SetUserScreen from "./screens/SetUser";
+import { HeaderShownContext } from "@react-navigation/elements";
+import {
+  SafeAreaFrameContext,
+  SafeAreaView,
+} from "react-native-safe-area-context";
 
-const Stack = createNativeStackNavigator();
+// define a tipagem das paginas e seus paremetros
+type TScreenDefinitions = {
+  home: undefined;
+  details: { rate: number };
+  setUserName: undefined;
+};
+
+const Stack = createNativeStackNavigator<TScreenDefinitions>();
 
 export default function Routes() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
+      <Stack.Navigator
+        initialRouteName="home"
+        screenOptions={{ headerShown: false }} // omit o header
+        screenLayout={({ children }) => <SafeAreaView>{children}</SafeAreaView>}
+      >
         <Stack.Screen
-          name="Home"
+          name="home"
           component={HomeScreen}
           options={{ title: "Home" }}
         />
-        <Stack.Screen name="Detalhes" component={DetailScreen} />
+        <Stack.Group
+          screenOptions={{ sheetCornerRadius: 24, presentation: "formSheet" }} // configura a borda da pagina e como a pagina é aberta
+        >
+          <Stack.Screen
+            name="details"
+            component={DetailScreen}
+            options={{
+              sheetAllowedDetents: [0.8, 0.95], // configura o tamanho da tela que vai ser exibida vai de 0 a 1
+            }}
+          />
+          <Stack.Screen
+            name="setUserName"
+            component={SetUserScreen}
+            options={{
+              sheetAllowedDetents: [0.4, 0.6],
+            }}
+          />
+        </Stack.Group>
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
+
+// tipagem para navegação entre paginas
+export type TNavigationProps = NativeStackNavigationProp<TScreenDefinitions>;
+
+// tipagem para os parametros da paginas
+export type TRouteProps<T extends keyof TScreenDefinitions> = RouteProp<
+  TScreenDefinitions,
+  T
+>;
